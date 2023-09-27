a9cd3ccab6801cf2baec6233c77cbfc1e6cd6f38
joshuaPhong <110524932+joshuaPhong@users.noreply.github.com>
2023-09-27 22:16:16 +1300

    working list view with bootstrap

--- display_data/views.py
+++ display_data/views.py
@@ -7,3 +7,9 @@ class ContinentConsumptionView(ListView):
     template_name = 'display_data/continent_energy_consumption.html'
     context_object_name = 'continent_consumption'
     ordering = ['year']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        years = ContinentConsumption.objects.values_list('year', flat=True)
+        context['years'] = years
+        return context
