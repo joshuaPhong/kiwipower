abf9620f514f74e0b5cc767133ab8bae43d8b2cd
joshuaPhong <110524932+joshuaPhong@users.noreply.github.com>
2023-10-07 12:43:41 +1300

    test renwable total power list view. plot path fails

--- display_data/tests/tests_views.py
+++ display_data/tests/tests_views.py
@@ -9,7 +9,7 @@ import io
 import matplotlib.pyplot as plt
 
 from display_data.models import NonRenewablesTotalPowerGenerated, \
-    RenewablePowerGenerated
+    RenewablePowerGenerated, RenewableTotalPowerGenerated
 
 
 class TestNonRenewablesTotalPowerListView(TestCase):
@@ -65,6 +65,11 @@ class TestNonRenewablesTotalPowerListView(TestCase):
         self.assertIn(column_name, df.columns)
         column_data = df[column_name].values[0]
         self.assertEqual(column_data, 'example_column')
+
+        # todo: test plot.failing
+        # if column_name:
+        #     self.assertTrue('/display_data/images/bar.png' in
+        #     response.context)
 
 
 class TestRenewablePowerGenerationListView(TestCase):
@@ -217,3 +222,54 @@ class TestRenewablePowerColumnView(TestCase):
         # self.assertTrue(plot.xlabel == 'Year')
         # self.assertTrue(plot.ylabel == 'Hydro')
         # self.assertTrue(plot.grid == True)
+
+
+class TestRenewablesTotalPowerListView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.view_url = reverse(
+            'renewables_total_power')
+        self.renewable_power = (
+            RenewableTotalPowerGenerated.objects.create(
+                mode_of_generation='hydro',
+                contribution_twh=100.0
+            ))
+
+    def test_view_accessible(self):
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(self.view_url)
+        self.assertTemplateUsed(response,
+                                'display_data'
+                                '/renewables_total_power_generated.html')
+
+    def test_view_has_correct_context_data(self):
+        response = self.client.get(self.view_url)
+        self.assertTrue('renewables_total_power' in response.context)
+
+    def test_queryset_is_dataframe(self):
+        query_set = RenewableTotalPowerGenerated.objects.all()
+        df = pd.DataFrame.from_records(query_set.values())
+        column_name = 'mode_of_generation'
+
+        self.assertTrue(isinstance(df, pd.DataFrame))
+        self.assertListEqual(list(df.columns),
+                             ['id', 'mode_of_generation', 'contribution_twh'])
+        self.assertEqual(df.loc[0, 'mode_of_generation'], 'hydro')
+        self.assertIn(column_name, df.columns)
+
+    def test_column_data(self):
+        query_set = RenewableTotalPowerGenerated.objects.all()
+        df = pd.DataFrame.from_records(query_set.values())
+        column_name = 'mode_of_generation'
+        response = self.client.get(self.view_url,
+                                   kwargs={'column_name': column_name})
+
+        self.assertEqual(df.loc[0, 'mode_of_generation'], 'hydro')
+        self.assertIn(column_name, df.columns)
+        column_data = df[column_name].values[0]
+        self.assertEqual(column_data, 'hydro')
+
+        # todo: test plot
